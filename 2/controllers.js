3b7ebd9d399d6a75d0e93ef2d08a5cc4108b8d92
mmgittraining <maja@miarecki.net>
2015-03-11 23:03:52 +0100

    Second example

---
+++ 2/controllers.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Maja on 2015-03-11.
+ */
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('CartController', function ($scope) {
+    $scope.items = [
+        {title: 'Paint pots', quantity: 8, price: 3.95},
+        {title: 'Polka dots', quantity: 17, price: 12.95},
+        {title: 'Pebbles', quantity: 5, price: 6.95}
+    ];
+    $scope.remove = function(index) {
+        $scope.items.splice(index, 1);
+    };
+});
